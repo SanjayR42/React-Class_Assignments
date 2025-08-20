2115671c9a624b86d22087d3f3160e92a9089931
SanjayR42 <157700826+SanjayR42@users.noreply.github.com>
2025-08-20 17:36:29 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import First from './components/First';
+import StudentTable from './components/StudentTable';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <First/>
-      <Footer/>
-      
+      <StudentTable/>
     </div>
   );
 }
